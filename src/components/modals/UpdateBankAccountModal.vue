847603d5f1e540f84a4db46986444671a3d2671b
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-05-12 18:53:16 +0200

    small start

---
+++ src/components/modals/UpdateBankAccountModal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="modal text-dark fade" :id="'SS' + account.iban">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Update Bank Account Settings</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <p class="text-danger">{{ errorMsg }}</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">
+            Update
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "UpdateBankAccountModal",
+    props: {
+        account: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
