e1d637ebcbe8d6c6da7082701d9df1fef763e4ad
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-06-06 13:29:04 +0200

    hi

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:80",
+        target: "https://floppa-inholland.herokuapp.com",
       },
     },
   },
