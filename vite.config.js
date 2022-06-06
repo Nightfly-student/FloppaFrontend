1dfbb473132f627c801e6687eb381c24df06f7c0
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-06-06 13:41:04 +0200

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+
+const defaultConfig = {
   plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://floppa-inholland.herokuapp.com",
-      },
-    },
-  },
+}
+// https://vitejs.dev/config/
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    
+    const isDev = mode === 'development'
+
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target:  isDev ? 'http://localhost:80' : "https://floppa-inholland.herokuapp.com",
+            changeOrigin: isDev,
+            secure: !isDev
+          }
+        }
+      }
+    }
+  } else {
+    return defaultConfig
+  }
 })
